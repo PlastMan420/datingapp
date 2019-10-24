d7072c1ccc5372c387c102a17d0fc576ed89bc4c
PlastMan420 <mostafa.alaa78@outlook.com>
2019-10-24 16:47:47 +0200

    fixed some mess

--- DatingApp-SPA/src/app/home/home.component.ts
+++ DatingApp-SPA/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  registerMode = false;
+  values: any;
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.getValues();
   }
-
+  registerToggle() {
+    this.registerMode = true;
+  }
+  getValues() {
+    this.http.get('http://localhost:5000/api/values').subscribe(response => {
+    this.values = response;
+    }, error => {
+      console.log(error);
+    });
+  }
+  cancelRegisterMode(registerMode: boolean) {
+    this.registerMode = registerMode;
+  }
 }
--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  @Input() valuesFromHome: any;
+  @Output() cancelRegister = new EventEmitter(); // output properties emmit events.
+  model: any = {};
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  register() {
+    console.log(this.model);
+  }
+ cancel() {
+  this.cancelRegister.emit(false); // we want to emit a 'false'
+  console.log('cancelled');
+ }
 }
